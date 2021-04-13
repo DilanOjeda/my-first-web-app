874ff4ba662d0161b05604577ea7dde6058752c7
DilanOjeda <dilanangel12@gmail.com>
2021-04-13 16:19:12 -0400

    this is my first nodejs project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(__dirname + '/public')); // the dependce of place you order (static always will be first)
+
+app.set('view engine', 'ejs'); 
+app.set('views' + __dirname + '/views')
+
+app.get('/', (req, res)=>{
+    res.render('index', {title: 'Introduction'});
+});
+app.get('/services', (req, res)=>{
+    res.render('services', {title: 'Services', message: 'This a message'});
+})
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {
+        title: 'Error 404',
+        message: 'The web page was not found'
+    });
+});
+
+app.listen(port, ()=>{
+    console.log(`the port is runing on port ${port}`);
+});
